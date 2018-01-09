0928c047300b82baa2f0e991abd612eb6fc26212
NikS8 <33399815+NikS8@users.noreply.github.com>
2018-01-09 17:15:21 +0300

    Add files via upload

---
+++ temperature_Collector_get.ino
@@ -0,0 +1,143 @@
+#include <OneWire.h>                    // Библиотека протокола 1-Wire
+#include <DallasTemperature.h>          // Библиотека для работы с датчиками DS*
+#define ONE_WIRE_BUS 9                 // Шина данных на 9 пине для датчиков DS18B20 (ардуино UNO)
+#define TEMPERATURE_PRECISION 9         // Точность измерений в битах (по умолчанию 12)
+OneWire oneWire(ONE_WIRE_BUS);            // Создаем экземпляр объекта протокола 1-WIRE - OneWire
+DallasTemperature sensorsDS(&oneWire);    // На базе ссылки OneWire создаем экземпляр объекта, работающего с датчиками DS*
+
+DeviceAddress sensorDsCollectorIn = { 0x28, 0xFF, 0x74, 0xF0, 0xB2, 0x16, 0x03, 0x1C };   // адрес датчика DS18B20 на входе в коллектор
+DeviceAddress sensorDsCollectorOut = { 0x28, 0xFF, 0x57, 0x1E, 0xB3, 0x16, 0x04, 0xDE };  // адрес датчика DS18B20 на выходе из коллектора
+DeviceAddress sensorDsKitchenOut1 = { 0x28, 0xFF, 0x63, 0x89, 0x01, 0x17, 0x05, 0x4B };    // адрес датчика DS18B20 на трубе №1 из летней кухни
+DeviceAddress sensorDsKitchenOut2 = { 0x28, 0xFF, 0x14, 0x17, 0x01, 0x17, 0x03, 0x5E };    // адрес датчика DS18B20 на трубе №2 из летней кухни
+
+int sensorDsTempCollectorIn;   // температура датчика DS18B20 на входе в коллектор
+int sensorDsTempCollectorOut;  // температура датчика DS18B20 на выходе из коллектора
+int sensorDsTempKitchenOut1;    // температура датчика DS18B20 на трубе №1 из летней кухни
+int sensorDsTempKitchenOut2;    // температура датчика DS18B20 на трубе №2 из летней кухни
+
+//-------------------------------------------------
+#include <SoftEasyTransfer.h>
+#include <SoftwareSerial.h>
+#define DIR 13            // переключатель прием\передача на Pin13
+
+SoftwareSerial mySerial(2, 3);      // Serial для RS485
+SoftEasyTransfer ETin, ETout;  //create two objects
+
+int ID = 2;              // номер этой ардуины
+struct SEND_DATA_STRUCTURE {                  // структура, которую будем передавать
+  int ID;
+  int sensorDsKitchenOut1;    // адрес датчика DS18B20 на трубе №1 из летней кухни
+  int sensorDsKitchenOut2;    // адрес датчика DS18B20 на трубе №2 из летней кухни
+  int sensorDsCollectorIn;   // адрес датчика DS18B20 на входе в коллектор
+  int sensorDsCollectorOut;  // адрес датчика DS18B20 на выходе из коллектора
+};
+
+struct RECEIVE_DATA_STRUCTURE {         // структура, которую будем принимать
+  //put your variable definitions here for the data you want to receive
+  //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
+  int ID;
+  char action;
+  int targetPin;
+  int levelPin;
+};
+
+char action;    //  "get"/"set" - запрос выдачи данных / команда на исполнение
+int levelPin;   //  установка уровня на Pin (LOW/HIGH)
+int targetPin;         //  Pin управления
+
+//give a name to the group of data
+RECEIVE_DATA_STRUCTURE rxdata;
+SEND_DATA_STRUCTURE txdata;
+//-----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+
+void setup() {
+  Serial.begin(9600);
+  //----------
+  mySerial.begin(9600); // start serial port
+  ETin.begin(details(rxdata), &mySerial); //  //start the library, pass in the data details and the name of the serial port
+  ETout.begin(details(txdata), &mySerial);
+
+  pinMode(DIR, OUTPUT);
+  delay(50);
+  digitalWrite(DIR, LOW);     // включаем прием
+  //----------
+  sensorsDS.begin(); // Запускаем поиск всех датчиков DS18B20 на шине
+
+  //------------
+
+}
+//------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------
+
+void loop() {
+  //----------------------------------
+  digitalWrite(DIR, LOW);                       // включаем прием
+
+
+  sensorsDS.requestTemperatures(); // Запускаем процесс измерение температуры на всех датчиках DS18B20
+  delay(30);
+  sensorDsTempKitchenOut1 = sensorsDS.getTempC(sensorDsKitchenOut1);    // температура датчика DS18B20 на трубе №1 из летней кухни
+  sensorDsTempKitchenOut2 = sensorsDS.getTempC(sensorDsKitchenOut2);    // температура датчика DS18B20 на трубе №2 из летней кухни
+  sensorDsTempCollectorIn = sensorsDS.getTempC(sensorDsCollectorIn);   // температура датчика DS18B20 на входе в коллектор
+  sensorDsTempCollectorOut = sensorsDS.getTempC(sensorDsCollectorOut);  // температура датчика DS18B20 на выходе из коллектора
+
+  //-------------
+  if (ETin.receiveData()) {                         // если пришли данные
+    byte id = rxdata.ID; // читаем байт, в нем для кого этот пакет
+
+    if (id == ID) { // и если пакет пришел нам
+
+      if (rxdata.action == "set") {
+        targetPin = rxdata.targetPin;
+        levelPin = rxdata.levelPin;
+
+        pinMode(targetPin, OUTPUT);
+        delay(50);
+        analogWrite(targetPin, levelPin);
+
+      }
+      else
+      {
+        //      ID = 2;  // номер этой ардуины
+        txdata.ID = ID;
+        txdata.sensorDsKitchenOut1 = sensorDsTempKitchenOut1;    // температура датчика DS18B20 на трубе №1 из летней кухни
+        txdata.sensorDsKitchenOut2 = sensorDsTempKitchenOut2;    // температура датчика DS18B20 на трубе №2 из летней кухни
+        txdata.sensorDsCollectorIn = sensorDsTempCollectorIn;   // температура датчика DS18B20 на входе в коллектор
+        txdata.sensorDsCollectorOut = sensorDsTempCollectorOut;  // температура датчика DS18B20 на выходе из коллектора
+
+        digitalWrite(DIR, HIGH);                      // включаем передачу
+        delay(50);
+
+        ETout.sendData();                              // отправляем
+        delay(50);
+        // небольшая задержка, иначе неуспевает
+        /////////////////
+        Serial.print(" ID = ");
+        Serial.print(txdata.ID);                 // и отправляем в Serial
+        Serial.print("  ");
+
+        Serial.print(" txdata.sensorDsCollectorIn = ");
+        Serial.print(txdata.sensorDsCollectorIn);
+        Serial.print(" txdata.sensorDsCollectorOut = ");
+        Serial.print(txdata.sensorDsCollectorOut);                 // и отправляем в Serial
+        Serial.print(" txdata.sensorDsKitchenOut1 = ");
+        Serial.print(txdata.sensorDsKitchenOut1);                 // и отправляем в Serial
+        Serial.print(" txdata.sensorDsKitchenOut2 = ");
+        Serial.print(txdata.sensorDsKitchenOut2);
+
+        Serial.println();
+        delay(10);                                     // небольшая задержка, иначе неуспевает
+      }
+    }
+  }
+  //----------
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//			Дополнительные
+//				функции
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
